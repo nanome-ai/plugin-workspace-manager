86e95b9adafd3db0a3e76f65a74e634a1f7f4a56
Alex McNerney <alex@nanome.ai>
2019-09-03 16:51:30 -0700

    Added user accounts support

--- nanome_workspace_manager/WorkspaceManager.py
+++ nanome_workspace_manager/WorkspaceManager.py
@@ -11,12 +11,18 @@ import os
 import zlib
 import traceback
 from timeit import default_timer as timer
+from hashlib import md5
+import re
 
 # This plugin uses undocumented network code, in order to reuse already available serialization code
 
-workspace_dir = os.path.join(os.path.dirname(__file__), 'Workspaces')
-if not os.path.exists(workspace_dir):
-    os.makedirs(workspace_dir)
+DIR = os.path.dirname(__file__)
+AUTH_PATH = os.path.join(DIR, 'accounts.txt')
+MENU_PATH = os.path.join(DIR, 'menu.json')
+
+WORKSPACE_DIR = os.path.join(DIR, 'Workspaces')
+if not os.path.exists(WORKSPACE_DIR):
+    os.makedirs(WORKSPACE_DIR)
 
 workspace_serializer = _WorkspaceSerializer()
 dictionary_serializer = _DictionarySerializer()
@@ -31,11 +37,11 @@ class WorkspaceManager(nanome.PluginInstance):
     ###################################
 
     def open_file_for_save(self, name):
-        file_path = os.path.join(workspace_dir, name)
+        file_path = os.path.join(WORKSPACE_DIR, name)
         if os.path.exists(file_path):
             self.send_notification(NotificationTypes.error, "Workspace name already exists")
-            self.save.unusable = False
-            self.update_content(self.save)
+            self.btn_save.unusable = False
+            self.update_content(self.btn_save)
             return
 
         file = open(file_path, "wb")
@@ -45,8 +51,8 @@ class WorkspaceManager(nanome.PluginInstance):
             except:
                 self.send_notification(NotificationTypes.error, "Failed to save workspace, check plugin")
                 Logs.error("Failed to save workspace", traceback.format_exc())
-            self.save.unusable = False
-            self.update_content(self.save)
+            self.btn_save.unusable = False
+            self.update_content(self.btn_save)
             file.close()
             self.refresh_menu()
             self.__timer = timer()
@@ -67,8 +73,10 @@ class WorkspaceManager(nanome.PluginInstance):
         compressed_data = zlib.compress(to_write)
         file.write(compressed_data)
 
+        self.send_notification(NotificationTypes.success, "Workspace saved")
+
     def open_file_for_load(self, name):
-        file_path = os.path.join(workspace_dir, name)
+        file_path = os.path.join(WORKSPACE_DIR, name)
         try:
             file = open(file_path, "rb")
         except:
@@ -98,94 +106,74 @@ class WorkspaceManager(nanome.PluginInstance):
 
         self.update_workspace(workspace)
 
+        self.send_notification(NotificationTypes.success, "Workspace loaded")
+
     ###################################
     ### UI
     ###################################
 
     def create_menu(self):
+        self.menu = nanome.ui.Menu.io.from_json(MENU_PATH)
         menu = self.menu
-        menu.title = "Workspace Manager"
-        menu._width = 0.8
-        menu._height = 0.7
-        menu.enabled = True
 
-        ln_list = menu.root.create_child_node()
-        ln_footer = menu.root.create_child_node()
-        ln_name = ln_footer.create_child_node()
-        ln_save = ln_footer.create_child_node()
+        # pages + prefabs
+        self.pg_auth = menu.root.find_node('AuthPage')
+        self.pg_main = menu.root.find_node('MainPage')
+        self.pfb_item = menu.root.find_node('ItemPrefab')
 
-        ln_list.forward_dist = 0.01
-        ln_list.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_footer.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_list.sizing_value = 0.85
-        ln_footer.sizing_value = 0.15
-        ln_footer.layout_orientation = LayoutNode.LayoutTypes.horizontal
-        ln_name.forward_dist = 0.01
-        ln_name.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_save.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_name.sizing_value = 0.7
-        ln_save.sizing_value = 0.3
+        # auth elements
+        self.in_user = self.pg_auth.find_node('Username').get_content()
+        self.in_pass = self.pg_auth.find_node('Password').get_content()
+        self.btn_auth = self.pg_auth.find_node('Enter').get_content()
+        self.btn_auth.register_pressed_callback(self.auth_callback)
 
-        self.list = ln_list.add_new_list()
-        self.field = ln_name.add_new_text_input()
-        self.field.placeholder_text = "Workspace name"
-        self.save = ln_save.add_new_button(text="Save")
-        self.save.register_pressed_callback(self.save_pressed_callback)
-
-        item = LayoutNode()
-        ln_label = item.create_child_node(name="file_name")
-        ln_load = item.create_child_node(name="file_load")
-        ln_delete = item.create_child_node(name="file_delete")
-        label = ln_label.add_new_label()
-        load = ln_load.add_new_button(text="Load")
-        delete = ln_delete.add_new_button(text="Delete")
-        item.layout_orientation = LayoutNode.LayoutTypes.horizontal
-        ln_label.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_load.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_delete.sizing_type = LayoutNode.SizingTypes.ratio
-        ln_label.sizing_value = 0.6
-        ln_load.sizing_value = 0.2
-        ln_delete.sizing_value = 0.2
-        self.item = item
+        # main elements
+        self.ls_workspaces = self.pg_main.find_node('List').get_content()
+        self.in_name = self.pg_main.find_node('Name').get_content()
+        self.btn_save = self.pg_main.find_node('Save').get_content()
+        self.btn_save.register_pressed_callback(self.save_callback)
 
         self.update_menu(menu)
 
     def refresh_menu(self):
-        files = [filename for filename in os.listdir(workspace_dir)]
-        file_names = set(map(lambda item: item.name, self.list.items))
+        if self.user == None:
+            return
+        
+        files = self.get_workspaces()
+        file_names = set(map(lambda item: item.name, self.ls_workspaces.items))
         add_set = set(files)
         remove_files = file_names - add_set
         changed = False
 
         for file_name in remove_files:
             item_to_delete = None
-            for item in self.list.items:
+            for item in self.ls_workspaces.items:
                 if item.name == file_name:
                     item_to_delete = item
                     break
             if item_to_delete != None:
-                self.list.items.remove(item_to_delete)
+                self.ls_workspaces.items.remove(item_to_delete)
             changed = True
 
         add_files = add_set - file_names
         for file_name in add_files:
-            new_item = self.item.clone()
+            new_item = self.pfb_item.clone()
             new_item.name = file_name
-            label = new_item.find_node("file_name").get_content()
-            load = new_item.find_node("file_load").get_content()
-            delete = new_item.find_node("file_delete").get_content()
-            label.text_value = file_name
+            label = new_item.find_node("Label").get_content()
+            load = new_item.get_content()
+            delete = new_item.find_node("Delete").get_content()
+            label.text_value = file_name if self.admin else file_name.split(' - ', 1)[1]
             changed = True
 
             load.workspace = file_name
             delete.workspace = file_name
-            load.register_pressed_callback(self.load_pressed_callback)
-            delete.register_pressed_callback(self.delete_pressed_callback)
+            load.register_pressed_callback(self.load_callback)
+            delete.register_pressed_callback(self.delete_callback)
 
-            self.list.items.append(new_item)
+            self.ls_workspaces.items.append(new_item)
 
         if changed:
-            self.update_content(self.list)
+            self.update_content(self.ls_workspaces)
 
     def display_menu(self):
         self.menu.enabled = True
@@ -196,8 +184,11 @@ class WorkspaceManager(nanome.PluginInstance):
     ###################################
 
     def start(self):
+        self.user = None
+        self.admin = False
+
+        self.load_accounts()
         self.create_menu()
-        self.refresh_menu()
         self.__timer = timer()
 
     def on_run(self):
@@ -207,22 +198,64 @@ class WorkspaceManager(nanome.PluginInstance):
         if timer() - self.__timer >= 5.0:
             self.refresh_menu()
             self.__timer = timer()
+    
+    def load_accounts(self):
+        with open(AUTH_PATH, 'r') as f:
+            self.accounts = [line.rstrip().split(' ') for line in f.readlines()]
+    
+    def save_accounts(self):
+        with open(AUTH_PATH, 'w') as f:
+            f.writelines('%s\n' % ' '.join(account) for account in self.accounts)
+    
+    def clean_filename(self, filename):
+        # only allow filename to contain alphanum, "-", "_", and " "
+        return re.sub('[^a-zA-Z0-9-_ ]', '', filename)
 
-    def save_pressed_callback(self, button):
-        name = self.field.input_text.strip()
+    def get_workspaces(self):
+        files = [filename for filename in os.listdir(WORKSPACE_DIR)]
+        if not self.admin:
+            files = list(filter(lambda f: f.split(' - ')[0] == self.user, files))
+        return files
+
+    ###################################
+    ### Button Callbacks
+    ###################################
+    
+    def auth_callback(self, button):
+        username = self.in_user.input_text
+        password = md5(self.in_pass.input_text.encode()).hexdigest()
+
+        for account in self.accounts:
+            if username == account[0] and password == account[1]:
+                self.user = username
+                self.admin = account[2] == '1'
+                break
+
+        if self.user:
+            self.send_notification(NotificationTypes.success, "Welcome " + username)
+            self.pg_auth.enabled = False
+            self.pg_main.enabled = True
+            self.refresh_menu()
+            self.display_menu()
+        else:
+            self.send_notification(NotificationTypes.error, "Invalid login")
+
+    def save_callback(self, button):
+        name = self.clean_filename(self.in_name.input_text.strip())
         if name == '':
             self.send_notification(NotificationTypes.error, "Workspace name cannot be empty")
             return
-        self.save.unusable = True
-        self.update_content(self.save)
+        name = self.user + ' - ' + name
+        self.btn_save.unusable = True
+        self.update_content(self.btn_save)
         self.open_file_for_save(name)
 
-    def load_pressed_callback(self, button):
+    def load_callback(self, button):
         self.open_file_for_load(button.workspace)
 
-    def delete_pressed_callback(self, button):
+    def delete_callback(self, button):
         try:
-            os.remove(os.path.join(workspace_dir, button.workspace))
+            os.remove(os.path.join(WORKSPACE_DIR, button.workspace))
         except:
             pass
         self.refresh_menu()
